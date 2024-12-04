f25675f75d09250f2adc51c0ffebd259cd110b4b
justallyt <albertjunk2030@gmail.com>
2024-12-04 15:37:38 +0300

    Updated Fix

--- server/controllers/paymentsController.js
+++ server/controllers/paymentsController.js
@@ -1,21 +1,27 @@
 import asyncHandler from "express-async-handler";
 import Customer from "../models/Customer.js";
-const  xmlData = `<?xml version=\"1.0\" encoding=\"utf-8\"?>
+import dotenv from "dotenv";
+import axios from "axios";
+
+//initialize .env file
+dotenv.config();
+
+const  xmlData = `<?xml version="1.0" encoding="utf-8"?>
         <API3G>
-              <CompanyToken>57466282-EBD7-4ED5-B699-8659330A6996</CompanyToken>
+              <CompanyToken>B3F59BE7-0756-420E-BB88-1D98E7A6B040</CompanyToken>
               <Request>createToken</Request>
               <Transaction>
                     <PaymentAmount>450.00</PaymentAmount>
-                    <PaymentCurrency>USD</PaymentCurrency>
+                    <PaymentCurrency>KES</PaymentCurrency>
                     <CompanyRef>49FKEOA</CompanyRef>
-                    <RedirectURL>http://www.domain.com/payurl.php</RedirectURL>
-                    <BackURL>http://www.domain.com/backurl.php </BackURL>
+                    <RedirectURL>http://localhost:5173/checkout/billing-confirmation</RedirectURL>
+                    <BackURL>http://localhost:5173/checkout/billing-confirmation </BackURL>
                     <CompanyRefUnique>0</CompanyRefUnique>
                     <PTL>5</PTL>
              </Transaction>
              <Services>
                    <Service>
-                         <ServiceType>45</ServiceType>
+                         <ServiceType>85325</ServiceType>
                          <ServiceDescription>Flight from Nairobi to Diani</ServiceDescription>
                          <ServiceDate>2013/12/20 19:00</ServiceDate>
                    </Service>
@@ -39,16 +45,22 @@ export const InitiatePaymentWithCard = asyncHandler(async(req, res) => {
        } = req.body;
 
        //attempt to get customer if exists
-       const customerExists = await Customer.findOne({ email })
+      //  const customerExists = await Customer.findOne({ email })
 
-       console.log(req.body)
-
-       if(customerExists){
+      //  if(customerExists){
                
-       }else{
-             const name = `${firstname} ${lastname}`;
-             //const result = await Customer.create({ name, email, profileImage });
-       }
+      //  }else{
+      //        const name = `${firstname} ${lastname}`;
+      //        //const result = await Customer.create({ name, email, profileImage });
+      //  }
+
+      const config = {
+             headers: { "Content-Type": "text/xml"}
+      }
+
+     axios.post(process.env.DPO_ENDPOINT_URL, xmlData, config).then(res => {
+            console.log(res.data)
+     })
 
       res.status(201).json({ message: "This thing is working thanks."})
 })
